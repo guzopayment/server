b7f0bc9236a44f83259b4ac2d3994f68e1d8ac0d
semahegn-tilahun <yegnabetsew@gmail.com>
2026-02-20 17:14:47 +0300

    Delet garbages, delet reected , dropdown list

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ import reportRoutes from "./routes/reports.js";
 import adminRoutes from "./routes/admin.js";
 import adminDebugRoutes from "./routes/adminDebug.js";
 
+import adminCleanupRoutes from "./routes/adminCleanup.js";
+
 import { initSocket } from "./utils/socket.js";
 import fs from "fs";
 
@@ -48,6 +50,7 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/debug", adminDebugRoutes);
 
+app.use("/api/admin", adminCleanupRoutes);
 // ✅ Global error handler (so you see real errors instead of silent 500)
 app.use((err, req, res, next) => {
   console.error("🔥 GLOBAL ERROR:", err);
